6f48154e0aaad0beb69193931f1f7b03484b5e4e
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-28 11:37:23 -0500

    adds some file structure

---
+++ src/components/Root/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//angry cat
+//https://www.google.com/search?q=cat+adopt+me+meme&tbm=isch&source=iu&ictx=1&fir=E0JdHD-O7E2cnM%252CjGNC7rt-YZoVaM%252C_&vet=1&usg=AI4_-kS-Sqbns9q8cagnDmhPJPqW2QVjLw&sa=X&ved=2ahUKEwiE2JKQqb7rAhWFQs0KHb0BBm4Q9QF6BAgJEBc#imgrc=E0JdHD-O7E2cnM
+
+function Root() {
+  return <div>
+    <h1>Petful</h1>
+  </div>;
+}
+
+export default Root;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import './index.css'
+import './index.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Root from './root/Root'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from './components/Root';
 
-ReactDOM.render(<Root />, document.getElementById('root'))
+ReactDOM.render(<Root />, document.getElementById('root'));
